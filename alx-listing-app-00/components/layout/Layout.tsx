a5fbdfee2d249259a39984b0857ceadf7ea10322
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-12-01 01:44:09 +0300

    first push

---
+++ alx-listing-app-00/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+// components/layout/Layout.tsx
+import React from "react";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-gray-50">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
